3849c756f71b6cc7a502697bb50c76c52ded0bf9
yzdnaufan <ahyaznau@gmail.com>
2022-08-07 22:38:04 +0700

    feat : MVP

--- scrape.py
+++ scrape.py
@@ -1,17 +1,17 @@
-from cgi import print_arguments
-from nis import cat
+from asyncio.windows_events import NULL
 import requests
 from bs4 import BeautifulSoup
 from time import sleep
 
+import random
+
 import os
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 import time
 
-obj_ =[]
 
-
+def seed( s=100 ) :
+    random.seed(s)
+    
 class url:
     def __init__(self, judul, id, kategori, url_ ):
         self.judul = judul
@@ -20,8 +20,9 @@ class url:
         self.url_ = url_
         
 
+
 novelID = []
-novel =[]
+judul =[]
 inti = []
 kategori = []
 
@@ -42,39 +43,88 @@ with requests.Session() as s:
     
     
     
-def ListingsData(cats):
+def ListingsData(cats, i):
+    '''
+    Returning object list with attributes :
+    :judul:
+    :id:
+    :kategori:
+    :url_:
+    '''
     
-    soup = BeautifulSoup(cats, 'html.parser')
-    headlines = soup.find(class_='browse-content-container').find_all('a', attrs={"class": "title meta on-story-preview"})
-
-
+    obj_ =[]
+    with open('html/' +str(cats)+ '.html', 'r', encoding='utf-8') as o :
+        soup = BeautifulSoup(o, 'html.parser')
+        headlines = soup.find(class_='browse-content-container').find_all('a', attrs={"class": "title meta on-story-preview"})
+        
+        for int in range(i):
+            Url = url(headlines[int].text, headlines[int].get('data-story-id'), cats, 'https://wattpad.com'+ headlines[int].get('href'))
+            obj_.append(Url)
+        
     
-    
-    
-    return print(headlines[80]) 
-    
+    return obj_
 
 def Scrape(obj):
     
-    url = 'https://www.wattpad.com/' + str()
+    
+    url = obj.url_
+    
+    #Novel Openner
     response = s.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
-    story_title = soup.find(class_='browse-content').find_all('title meta on-story-preview')
-    for x in story_title:
-        web_ = x.find('a').get('href')
-        url1 = str(web_) +'?single=1'
+    
+    if soup.find(class_='story-parts') is None:
+        judul.append(obj.judul)         #judul novel/tulisan
+    
+        novelID.append(obj.id)          #ID wattpad novel/tulisan
+    
+        inti.append(None)             #text yang diambil
+    
+        kategori.append(obj.kategori)
         
+        return judul, novelID, inti, kategori
+    
+    
+    novel = soup.find(class_='story-parts').find_all('li')
+    
+    #random chapter pick
+    chapter = novel[random.randint(0, len(novel)-1)]
+    
+    
+    #locked chapter mitigation
+    #while chapter.find('title') is not None :
+    #    i = random.randint(0, len(novel)-1)
+    #    chapter = novel[i]
+    
+    #Chapter Opener
+    url = 'https://wattpad.com'+ str(chapter.find('a').get('href'))
+    response = s.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    
+    
+    
+    
+    text = soup.find(class_ = 'story-part').find_all('p')
+    
+    #get text paragraph randomized
+    p = text[random.randint(0, len(text)-1)]
+    
+    #unwanted text mitigation (e.g. Episode Text etc.)
+    #while p.get('style') is not None :
+    #    i = random.randint(0, len(text)-1)
+    #    p = text[i]
         
-        novel.append(obj.judul)     #judul novel/tulisan
-        
-        novelID.append(obj.id)      #ID wattpad novel/tulisan
-        
-        inti.append(toi_article.text)   #text yang diambil
-        
-        kategori.append(obj.kategori)    #kategori text/tulisan
-        
+    
+    judul.append(obj.judul)         #judul novel/tulisan
+    
+    novelID.append(obj.id)          #ID wattpad novel/tulisan
+    
+    inti.append(p.text)             #text yang diambil
+    
+    kategori.append(obj.kategori)    #kategori text/tulisan
+    
     sleep(5)
-        
-    return novel, novelID, inti, kategori
+    
+    return judul, novelID, inti, kategori
 
         
